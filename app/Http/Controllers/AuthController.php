7d66ce5edbf1387f365d389c43017fb96e8b9d8c
DionitaTh <164007834+DionitaTh@users.noreply.github.com>
2024-04-12 16:44:51 +0200

    7th

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,22 @@ class AuthController extends Controller
     public function login()
     {
         if (!empty(Auth::check())) {
-            return redirect('admin/dashboard');
+                        if(Auth::user()->user_type==1)
+            {
+                return redirect('admin/dashboard');
+            }
+            else if(Auth::user()->user_type==2)
+            {
+                return redirect('teacher/dashboard');
+            }
+            else if(Auth::user()->user_type==3)
+            {
+                return redirect('student/dashboard');
+            }
+            else if(Auth::user()->user_type==4)
+            {
+                return redirect('parent/dashboard');
+            }
         }
         return view('auth.login');
     }
@@ -22,9 +37,24 @@ class AuthController extends Controller
         $remember = !empty($request->remember) ? true : false;
         
         // Attempt to authenticate the user
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], true)) {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember)) {
           
-            return redirect('admin/dashboard');
+            if(Auth::user()->user_type== 1)
+            {
+                return redirect('admin/dashboard');
+            }
+            else if(Auth::user()->user_type== 2)
+            {
+                return redirect('teacher/dashboard');
+            }
+            else if(Auth::user()->user_type== 3)
+            {
+                return redirect('student/dashboard');
+            }
+            else if(Auth::user()->user_type== 4)
+            {
+                return redirect('parent/dashboard');
+            }
         } 
         else
         {        
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,60 @@
 <?php
+namespace App\Http\Middleware;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 
-Route::get('/', [AuthController::class, 'login']);
+Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'AuthLogin']);
 Route::get('logout', [AuthController::class, 'logout']);
 
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+});
 
 Route::get('admin/dashboard', function () {
     return view('admin.dashboard');
 });
 
 
-Route::get('admin/admin/list', function () {
-    return view('admin.admin.list');
+Route::group(['middleware' => AdminMiddleware::class], function (){
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+// Route::group(['middleware' => 'teacher'], function (){
+//     Route::get('teacher/dashboard', function () {
+//         return view('admin.dashboard');
+//     });
+// });
+
+// Route::group(['middleware' => 'student'], function (){
+//     Route::get('student/dashboard', function () {
+//         return view('admin.dashboard');
+//     });
+// });
+
+// Route::group(['middleware' => 'parent'], function (){
+//     Route::get('parent/dashboard', function () {
+//         return view('admin.dashboard');
+//     });
+// });
+Route::group(['middleware' => TeacherMiddleware::class], function () {
+    Route::get('teacher/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => StudentMiddleware::class], function () {
+    Route::get('student/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => ParentMiddleware::class], function () {
+    Route::get('parent/dashboard', function () {
+        return view('admin.dashboard');
+    });
 });
